393373cfa2fbafa496c7a646514d59fae8162674
emon84090 <emon84090@gmail.com>
2022-04-29 15:19:13 +0600

    folder structure done

---
+++ src/components/homepage/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <>
+            <section id='banner' className='w-full h-screen bg-gray-50'>
+
+            </section>
+        </>
+    );
+};
+
+export default Banner;
---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './Banner';
+
+const Homepage = () => {
+    return (
+        <>
+            <Banner></Banner>
+        </>
+    );
+};
+
+export default Homepage;
